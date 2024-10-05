407dc35c46b4ea565934b62fb4b4a4f4a6122273
clxxiii <clxxiii@users.noreply.github.com>
2024-10-05 16:12:08 -0400

    Home page stylistic changes
    Co-authored-by: Mio <Mio-Mahoney@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,49 @@
-<script>
-    let roomCode = '';
-    function join() {
-        if(roomCode == '') {
-        alert("Please enter a room code...")
-    } else {
-        alert("Joining the the room")
-    } 
-}
+<script lang="ts">
+	let acceptableCharacters = 'ABCDEFGHGJLMNPQRSTUVWXYZ0123456789';
 
-    function createRoom() {
-        alert("creating room...")
-    }
+	let roomCode = '';
+	function join() {
+		if (roomCode == '') {
+			alert('Please enter a room code...');
+		} else {
+			alert('Joining the the room: ' + roomCode);
+		}
+	}
+
+	function createRoom() {
+		alert('creating room...');
+	}
+
+	function formValidation() {
+		console.log(roomCode);
+		roomCode = roomCode.toUpperCase();
+		roomCode = roomCode
+			.split('')
+			.filter((x) => acceptableCharacters.includes(x))
+			.join('');
+		if (roomCode.length > 6) {
+			roomCode = roomCode.substring(0, roomCode.length - 1);
+		}
+	}
 </script>
 
-<div class=" bg-vanilla-100 w-screen h-screen text">
-
-<h1 class="text-xl flex flex-col justify-center items-center">Enter a room code</h1>
-<div id="centered content" class="flex justify-center items-center min-h-screen">
-<input bind:value={roomCode} placeholder="Room Code" class="border ">
-<button on:click={join} class="border bg-slate- px-4 rounded bg-moonstone-100">Join</button> 
-<button class="border rounded px-4 bg-tangerine-500" on:click={createRoom}>Create Room</button>
-</div>
+<div class="bg-vanilla-100 w-screen h-screen font-sans">
+	<h1 class="text-xl flex flex-col justify-center items-center">Enter a room code</h1>
+	<div id="centered content" class="grid grid-cols-2 gap-y-5 gap-x-5 w-fit item-center">
+		<input
+			bind:value={roomCode}
+			on:input={formValidation}
+			placeholder="Enter a Code"
+			class="bg-white/75 outline-none focus:border-slate-500 focus:bg-white border-slate-400 border-2 col-start-1 col-end-3 h-[70px] text-[40px] text-center"
+		/>
+		<button
+			on:click={join}
+			class="border px-4 rounded bg-moonstone-100 hover:bg-moonstone-200 transition active:bg-moonstone-300 row-start-2 row-end-3 col-start-1 col-end-2 p-2"
+			>Join</button
+		>
+		<button
+			class="border rounded px-4 bg-tangerine-400 hover:bg-tangerine-500 transition active:bg-tangerine-600 row-start-2 row-end-3 col-start-2 col-end-3 p-2"
+			on:click={createRoom}>Create Room</button
+		>
+	</div>
 </div>
