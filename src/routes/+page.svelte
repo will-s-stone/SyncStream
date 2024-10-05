1e45c6ac45734a5d48359ce40cf9cf12aba11594
Mio Mahoney <mmahone5@oswego.edu>
2024-10-05 15:24:02 -0400

    Updated merge

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <script>
     let roomCode = '';
     function join() {
@@ -14,19 +13,12 @@
     }
 </script>
 
-<div class=" bg- w-screen h-screen text-B5E2FA">
+<div class=" bg-vanilla-100 w-screen h-screen text">
 
 <h1 class="text-xl flex flex-col justify-center items-center">Enter a room code</h1>
 <div id="centered content" class="flex justify-center items-center min-h-screen">
 <input bind:value={roomCode} placeholder="Room Code" class="border ">
-<button on:click={join} class="border bg-slate- px-4 rounded">Join</button> 
-<button class="border rounded px-4" on:click={createRoom}>Create Room</button>
+<button on:click={join} class="border bg-slate- px-4 rounded bg-moonstone-100">Join</button> 
+<button class="border rounded px-4 bg-tangerine-500" on:click={createRoom}>Create Room</button>
 </div>
 </div>
-=======
-<h1 class="bg-moon">Enter a room code</h1>
-<input />
-<button>Join</button>
-
-<button>Create Room</button>
->>>>>>> 51c23182a1d001a94bfcdc7ab635f865506b6770
