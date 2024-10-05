9a7860725522d5ea06d90b5b0dc7e472b99b0264
Mio Mahoney <mmahone5@oswego.edu>
2024-10-05 15:18:56 -0400

    updated homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,24 @@
-<h1 class="">Enter a room code</h1>
-<input>
-<button>Join</button> 
+<script>
+    let roomCode = '';
+    function join() {
+        if(roomCode == '') {
+        alert("Please enter a room code...")
+    } else {
+        alert("Joining the the room")
+    } 
+}
 
-<button>Create Room</button>
+    function createRoom() {
+        alert("creating room...")
+    }
+</script>
+
+<div class=" bg- w-screen h-screen text-B5E2FA">
+
+<h1 class="text-xl flex flex-col justify-center items-center">Enter a room code</h1>
+<div id="centered content" class="flex justify-center items-center min-h-screen">
+<input bind:value={roomCode} placeholder="Room Code" class="border ">
+<button on:click={join} class="border bg-slate- px-4 rounded">Join</button> 
+<button class="border rounded px-4" on:click={createRoom}>Create Room</button>
+</div>
+</div>
